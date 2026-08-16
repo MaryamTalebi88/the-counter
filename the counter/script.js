2063a15620e50e0f8eff43a2cc7192c0cef789f0
Maryam <mt5012612@gmail.com>
2026-08-16 17:02:40 +0330

    Add files via upload

---
+++ the counter/script.js
@@ -0,0 +1,12 @@
+const maryam=document.querySelector('.maryam');
+maryam.addEventListener('click',function namr() {
+    maryam.style.color="red";
+}
+);
+const hell=document.querySelector('.head');
+hell.addEventListener('mouseover',function name(params) {
+    hell.style.color="red";
+})
+hell.addEventListener('mouseout',function name(params) {
+    hell.style.color="blue";
+})
